4f698a152b07ce1d00664aecaa495f256541e62d
Sakhman <68939201+Sakhman@users.noreply.github.com>
2020-11-11 08:07:59 +0530

    Update hq.py

--- hq.py
+++ hq.py
@@ -85,10 +85,10 @@ def show_not_on():
             print(x_in)
             print(x_indi)
             print(prize)
-            embed = Embed(title="**__Next Game Details !__**", description=f"**📆〢Date — {x_indi}\n⏰〢Time — {x_in} [I.S.T]\n💰〢Prize Money — {prize}** 🎉", color=0xff5733)
-            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/765632168650866750/770460404714962974/IMG_20200920_211614.jpg")
-            embed.set_footer(text="HQ Trivia Show | Subrata#3297", icon_url="https://cdn.discordapp.com/avatars/660337342032248832/828f7b13ce161e8a9d4c129e0ac776c4.webp?size=1024")
-            hook.send(embed=embed)
+            #embed = Embed(title="**__Next Game Details !__**", description=f"**📆〢Date — {x_indi}\n⏰〢Time — {x_in} [I.S.T]\n💰〢Prize Money — {prize}** 🎉", color=0xff5733)
+            #embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/765632168650866750/770460404714962974/IMG_20200920_211614.jpg")
+            #embed.set_footer(text="HQ Trivia Show | Subrata#3297", icon_url="https://cdn.discordapp.com/avatars/660337342032248832/828f7b13ce161e8a9d4c129e0ac776c4.webp?size=1024")
+            #hook.send(embed=embed)
 
 
 
