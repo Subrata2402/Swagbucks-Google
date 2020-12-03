a45a53a8fb3ae1521788bc71b87ef7686c4f3eca
Sakhman <68939201+Sakhman@users.noreply.github.com>
2020-12-03 18:24:01 +0530

    Update hq.py

--- hq.py
+++ hq.py
@@ -78,12 +78,13 @@ def show_not_on():
             x =  tm.strftime("%H:%M:%S [%d/%m/%Y] ")
             x_ind = tm.astimezone(timezone("Asia/Kolkata"))
             x_in = x_ind.strftime("%H:%M:%S [%d/%m/%Y] ")
-    
+            payout = (round(485/100))
+      
             prize = (response_data["nextShowPrize"])
             #time.sleep(5)
             print(x_in)
             print(prize)
-            embed = Embed(title=f"{tim} | {tm}", description=f"**Next Game Starts In**\n**{x_in}**", color=0x000000)
+            embed = Embed(title=payout, description=f"**Next Game Starts In**\n**{x_in}**", color=0x000000)
             embed.add_field(name="Next Show Prize", value=f"**{prize}**",inline=True)
             embed.set_image(url="https://cdn.discordapp.com/attachments/649457795875209265/672845602824126494/Nitro_2.gif")
             embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/578379566544846901/630400208265805835/401ec468afa82a2937b8ad3a4e811463.jpg")
