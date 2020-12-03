bfd331e8cb7f03c4b0d3897d65d3d21d9f7152b9
Sakhman <68939201+Sakhman@users.noreply.github.com>
2020-12-03 18:45:18 +0530

    Update hq.py

--- hq.py
+++ hq.py
@@ -78,7 +78,8 @@ def show_not_on():
             x =  tm.strftime("%H:%M:%S [%d/%m/%Y] ")
             x_ind = tm.astimezone(timezone("Asia/Kolkata"))
             x_in = x_ind.strftime("%H:%M:%S [%d/%m/%Y] ")
-            payout = str(5000/3658)
+            hp = "2463"
+            payout = str(5000/hp)
       
             prize = (response_data["nextShowPrize"])
             #time.sleep(5)
