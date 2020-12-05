7c158e957d3dd0cddc56a48c91973f434c4b2ff1
Sakhman <68939201+Sakhman@users.noreply.github.com>
2020-12-06 02:09:15 +0530

    Update hq.py

--- hq.py
+++ hq.py
@@ -201,6 +201,8 @@ def connect_websocket(socket_url, auth_token):
                 print(colored(correct, "blue"))
                 print(advancing)
                 print(eliminated)
+                #hook.send(f"**Correct Answer -- {correct}**")
+                #hook.send(f"**Advancing -- {advancing}      Eliminating --- {eliminated}**")
                 embd=discord.Embed(title=f"**Question No. {qcnt} of {Fullcnt}**", description=f"**[{question}]({google_query})**\n**Correct Answer: {correct}** <:emoji_13:772843132093202443>",color=0x4286f4)
                 embd.add_field(name="**__Status !__**", value=f"**● Advancing Players: {advancing}**\n**● Eliminated  Players: {eliminated}**", inline=True)
                 embd.set_footer(text=f"HQ Google | Subrata#3297")
