af21b95b1848181675d3354c31d6a040f83831aa
Sakhman <68939201+Sakhman@users.noreply.github.com>
2020-12-16 07:05:11 +0530

    Update hq.py

--- hq.py
+++ hq.py
@@ -89,7 +89,8 @@ def show_not_on():
             #embed.set_image(url="https://cdn.discordapp.com/attachments/649457795875209265/672845602824126494/Nitro_2.gif")
            # embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/578379566544846901/630400208265805835/401ec468afa82a2937b8ad3a4e811463.jpg")
             #embed.set_footer(text="Made By ⚘!ϻ.Captainᴼᴾ")
-            hook.send("@everyone Connected to HQ Websocket ✅")
+            embed=discord.Embed(title="**Connected to HQ Websocket** ✅", color=0x000000)
+            hook.send(embed=embed)
 
 
 
