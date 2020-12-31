45817749310889eb9429c66b9ca604c811561db9
Sakhman <68939201+Sakhman@users.noreply.github.com>
2021-01-01 04:34:03 +0530

    Update hq.py

--- hq.py
+++ hq.py
@@ -85,14 +85,14 @@ def show_not_on():
             time.sleep(5)
             print(x_in)
             print(prize)
-            #embed=discord.Embed(title=f"➜〢Date – {x_in}\n➜〢Time – {x_i}AM\n➜〢Prize Money – {prize}", color=0x00FBFF)
+            embed=discord.Embed(title=f"➜〢Date – {x_in}\n➜〢Time – {x_i}AM\n➜〢Prize Money – {prize}", color=0x00FBFF)
             #embed = Embed(title=f"HQ Trivia", description=f"**Next Game Starts In**\n**{x_in}**", color=0x000000)
             #embed.add_field(name="Next Show Prize", value=f"**{prize}**",inline=True)
             #embed.set_image(url="https://cdn.discordapp.com/attachments/649457795875209265/672845602824126494/Nitro_2.gif")
-            #embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/775384878942257173/794110961333174293/1024x1024bb.png")
-            #embed.set_footer(text="HQ Trivia Show | Subrata#3297")
+            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/775384878942257173/794110961333174293/1024x1024bb.png")
+            embed.set_footer(text="HQ Trivia Show | Subrata#3297")
             #embed=discord.Embed(title="**Connected to HQ Websocket** ✅", color=0x000000)
-            #hook.send(content="@everyone  **Next Game Details !**", embed=embed)
+            hook.send(content="@everyone  **Next Game Details !**", embed=embed)
 
 
 
