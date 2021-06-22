dc054f2267c6af503dae9323b3daeff389e853a9
Subrata2402 <68939201+Subrata2402@users.noreply.github.com>
2021-06-22 06:30:15 +0530

    Update hq.py

--- hq.py
+++ hq.py
@@ -20,7 +20,7 @@ import aniso8601
 import aiohttp
 import asyncio
 
-webhook_url="https://discordapp.com/api/webhooks/838837824614432849/Na_4Sl4PAbGQppbloqyrt6VfkkBIY2gkdttiy9Purd_1NNOmrKjc_PM-nprXm-yJLDmB"
+webhook_url="https://discordapp.com/api/webhooks/856696688437755934/9NW-2w46XZp-Sd2TmZuJTAg2hz7FVFPVDt4VY_7ZnXf_5yhjB1yQHGFe_Eh23KXMj8aM"
 
 try:
     hook = Webhook(webhook_url)
@@ -44,13 +44,11 @@ def show_not_on():
             raise e
 
     print("getting")
-    main_url = f"https://api-quiz.hype.space/shows/now?type="
-    headers = {"Authorization": f"Bearer {BEARER_TOKEN}",
-               "x-hq-client": "Android/1.3.0"}
-    # "x-hq-stk": "MQ==",
-    # "Connection": "Keep-Alive",
-    # "User-Agent": "okhttp/3.8.0"}
-
+    main_url = f"https://api.playswagiq.com/trivia/home?_uid="
+    headers = {
+               "Authorization": f"Bearer {BEARER_TOKEN}",
+               "user-agent": "SwagIQ-Android/34 (okhttp/3.10.0)"
+              }
     try:
         response_data = requests.get(main_url).json()
         print(response_data)
@@ -60,44 +58,32 @@ def show_not_on():
 
     logging.info(response_data)
 
-    if "broadcast" not in response_data or response_data["broadcast"] is None:
+    if "joinable" not in response_data or response_data["joinable"] is None:
         if "error" in response_data and response_data["error"] == "Auth not valid":
             raise RuntimeError("Connection settings invalid")
         else:
             print("Show not on.")
-            tim = (response_data["nextShowTime"])
-            tm = aniso8601.parse_datetime(tim)
-            x =  tm.strftime("%H:%M")
-            x_ind = tm.astimezone(timezone("Asia/Kolkata"))
-            x_in = x_ind.strftime("%d/%m/%Y")
-            x_inn = x_ind.strftime("%H:%M")
-    
-            prize = (response_data["nextShowPrize"])
-            time.sleep(5)
-            print(x_in)
-            print(prize)
-            embed=discord.Embed(title="Game is not Live", color=0x00ffff)
-            hook.send(embed=embed)
+            hook.send("Game Is Not Live!")
 
 
 
 def show_active():
-    main_url = 'https://api-quiz.hype.space/shows/now'
+    main_url = 'https://api.playswagiq.com/trivia/home?_uid='
     response_data = requests.get(main_url).json()
-    return response_data['active']
+    return response_data['joinable']
 
 
 def get_socket_url():
-    main_url = 'https://api-quiz.hype.space/shows/now'
+    main_url = 'https://api.playswagiq.com/trivia/join?_uid='
     response_data = requests.get(main_url).json()
-
-    socket_url = response_data['broadcast']['socketUrl'].replace('https', 'wss')
+    id = response_data["viewId"]
+    socket_url = f"wss://api.playswagiq.com/sock/1/game/{id}?_uid="
     return socket_url
 
 
 def connect_websocket(socket_url, auth_token):
     headers = {"Authorization": f"Bearer {auth_token}",
-               "x-hq-client": "iPhone8,2"}
+               "user-agent":"SwagIQ-Android/34 (okhttp/3.10.0)"}
 
 
     websocket = WebSocket(socket_url)
@@ -113,41 +99,7 @@ def connect_websocket(socket_url, auth_token):
             if message_data['type'] != 'interaction':
                 print(message_data)
         
-            if message_data['type'] == 'question':
-                question = message_data['question']
-                qcnt = message_data['questionNumber']
-                Fullcnt = message_data['questionCount']
-                embed=discord.Embed(title=f"Question {qcnt} out of {Fullcnt}", description=question, color=0x00ffff)
-                hook.send(embed=embed)
-                id1 = message_data["answers"][0]["answerId"]
-                id2 = message_data["answers"][1]["answerId"]
-                id3 = message_data["answers"][2]["answerId"]
-
-            elif message_data['type'] == 'answered':
-                name = message_data["username"]
-                ansid = message_data["answerId"]
-                if ansid == id1:
-                    embed = discord.Embed(title=f"{name} went Option - 1", color=0x00ff00)
-                    hook.send(embed=embed)
-                elif ansid == id2:
-                    embed = discord.Embed(title=f"{name} went Option - 2", color=0x00ff00)
-                    hook.send(embed=embed)
-                else:
-                    embed = discord.Embed(title=f"{name} went Option - 3", color=0x00ff00)
-                    hook.send(embed=embed)
-                
-
-            elif message_data["type"] == "questionClosed":
-                embed=discord.Embed(title="⏰ **| Time,s Up!**", color=0xa1fc03)
-                hook.send(embed=embed)
-
-"""
-def open_browser(question):
-
-    main_url = "https://www.google.co.in/search?q=" + question
-    webbrowser.open_new(main_url)
-"""
-
+            
 def get_auth_token():
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "BTOKEN.txt"), "r") as conn_settings:
         settings = conn_settings.read().splitlines()
